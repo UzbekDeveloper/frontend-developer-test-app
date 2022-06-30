fd9242e915759c34197872eb2d704549849228e7
Uzbek Developer <UzbekProgramist@gmail.com>
2022-06-30 18:29:58 +0500

    frontend-developer-test-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
+import { Layout } from "antd";
+import "./App.css";
+
+import Cards from "./Components/Cards";
+
+const { Header, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout className="Layout">
+        <Layout>
+          <Sider className="App-sider"></Sider>
+          <Header className="App-header" />
+          <Content style={{marginLeft: "200px"}}>
+            {/* Body */}
+            <div
+              className="title-margin"
+              style={{ display: "flex", flexDirection: "row" }}
+            >
+              <p>Заявки</p>
+              <p style={{ marginLeft: "8px", color: "#6D7471" }}>&sdot; 22</p>
+            </div>
+
+            {/* Cards */}
+            <Cards />
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   );
 }
